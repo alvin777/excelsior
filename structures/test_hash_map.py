8b31d68d1bd0b89f35a032f870de8d8bc94823b6
Alvin777 <alvin777@mail.ru>
2014-06-20 20:34:34 +0400

    [Change] Рефакторинг HashMap'а

--- structures/test_hash_map.py
+++ structures/test_hash_map.py
@@ -12,17 +12,15 @@ def random_generator(size):
         yield int(random.random()*sys.maxint)
 
 
-# LOAD_FACTOR_THRESHOLD = 0.75
-LOAD_FACTOR_THRESHOLD = 5
-
-class HashMap:
-    def __init__(self, allow_duplicates=True, initial_size=16):
+class ChainedHashMap:
+    def __init__(self, allow_duplicates=True, initial_size=16, load_factor_threshold=5):
         self.allow_duplicates = allow_duplicates
         self.hash_array_size = initial_size
         # self.hash_array = array.array('L', [0 for _ in xrange(self.hash_array_size)])
         self.hash_array = [None] * self.hash_array_size
         self.elements_count = 0
         self.load_factor = 0
+        self.load_factor_threshold = load_factor_threshold
 
     def calculate_hash(self, key):
         return hash(key)
@@ -58,37 +56,32 @@ class HashMap:
             self.elements_count += 1
             self.load_factor = self.elements_count/self.hash_array_size
         else:
-
-            if self.allow_duplicates:
-                linked_list.append((key, value))
-                self.elements_count += 1
-                self.load_factor = self.elements_count/self.hash_array_size
-            else:
-                found_key = False
+            found_key = False
+            if not self.allow_duplicates:
                 for i in xrange(len(linked_list)):
                     if linked_list[i][0] == key:
                         linked_list[i] = (key, value)
                         found_key = True
                         break
 
-                if not found_key:
-                    linked_list.append((key, value))
-                    self.elements_count += 1
-                    self.load_factor = self.elements_count/self.hash_array_size
+            if self.allow_duplicates or not found_key:
+                linked_list.append((key, value))
+                self.elements_count += 1
+                self.load_factor = self.elements_count/self.hash_array_size
 
-        if self.load_factor >= LOAD_FACTOR_THRESHOLD:
+        if self.load_factor >= self.load_factor_threshold:
             self.resize(self.hash_array_size*2)
 
     def __delitem__(self, key):
         key_hash = self.calculate_hash(key)
         key_index = key_hash % self.hash_array_size
+        linked_list = self.hash_array[key_index]
 
-        if self.hash_array[key_index] is not None:
-            list_len_before = len(self.hash_array[key_index])
-            self.hash_array[key_index][:] = [(stored_key, value) for (stored_key, value)
-                                            in self.hash_array[key_index] if not key == stored_key]
-            list_len_after = len(self.hash_array[key_index])
-            len_diff = list_len_before - list_len_after
+        if linked_list is not None:
+            list_len_before = len(linked_list)
+            linked_list[:] = [(stored_key, value) for (stored_key, value) in linked_list if not key == stored_key]
+            len_diff = list_len_before - len(linked_list)
+            
             self.elements_count -= len_diff
             self.load_factor = self.elements_count/self.hash_array_size
 
@@ -104,10 +97,9 @@ class HashMap:
         self.elements_count = 0
         self.load_factor = 0
 
-        for i in xrange(len(old_hash_array)):
-            if old_hash_array[i] is not None:
-                for j in xrange(len(old_hash_array[i])):
-                    self[old_hash_array[i][j][0]] = old_hash_array[i][j][1]
+        for linked_list in [x for x in old_hash_array if x is not None]:
+            for (key, value) in linked_list:
+                self[key] = value
 
     def stats(self):
         return [len(x) if x is not None else 0 for x in self.hash_array]
@@ -115,16 +107,18 @@ class HashMap:
 
 class TestSortingMethods(unittest.TestCase):
     def test_simple_hash_map(self):
-        hash_map = HashMap()
+        hash_map = ChainedHashMap()
         hash_map[1] = 10
         hash_map[2] = 20
 
         self.assertEqual(hash_map.size(), 2)
         self.assertEqual(hash_map[1], 10)
         self.assertEqual(hash_map[2], 20)
+        self.assertEqual(hash_map[3], None)
+        self.assertEqual(hash_map[17], None)
 
     def test_simple_collision(self):
-        hash_map = HashMap()
+        hash_map = ChainedHashMap(initial_size=100)
         hash_map[1] = 10
         hash_map[101] = 20
 
@@ -133,7 +127,7 @@ class TestSortingMethods(unittest.TestCase):
         self.assertEqual(hash_map[101], 20)
 
     def test_1000_elements(self):
-        hash_map = HashMap()
+        hash_map = ChainedHashMap()
         reference_dict = {}
 
         for x in xrange(1000):
@@ -150,7 +144,7 @@ class TestSortingMethods(unittest.TestCase):
         # print hash_map.stats()
 
     def test_1000_elements_limited(self):
-        hash_map = HashMap(allow_duplicates=False)
+        hash_map = ChainedHashMap(allow_duplicates=False)
         reference_dict = {}
 
         for x in xrange(1000):
@@ -167,7 +161,7 @@ class TestSortingMethods(unittest.TestCase):
         # print hash_map.stats()
 
     def test_restrict_duplicates(self):
-        hash_map = HashMap(allow_duplicates=False)
+        hash_map = ChainedHashMap(allow_duplicates=False)
         hash_map[1] = 10
         hash_map[1] = 20
 
@@ -175,7 +169,7 @@ class TestSortingMethods(unittest.TestCase):
         self.assertEqual(hash_map[1], 20)
 
     def test_deletion(self):
-        hash_map = HashMap()
+        hash_map = ChainedHashMap()
         hash_map[1] = 10
         hash_map[101] = 20
         del hash_map[101]
@@ -185,7 +179,7 @@ class TestSortingMethods(unittest.TestCase):
         self.assertEqual(hash_map[101], None)
 
     def test_deletion_restrict_duplicates(self):
-        hash_map = HashMap(allow_duplicates=False)
+        hash_map = ChainedHashMap(allow_duplicates=False)
         hash_map[1] = 10
         hash_map[101] = 20
         hash_map[101] = 30
@@ -196,13 +190,13 @@ class TestSortingMethods(unittest.TestCase):
         self.assertEqual(hash_map[101], None)
 
     def test_string_keys(self):
-        hash_map = HashMap()
+        hash_map = ChainedHashMap()
         hash_map['1'] = 10
         hash_map['2'] = 20
 
         self.assertEqual(hash_map.size(), 2)
         self.assertEqual(hash_map['1'], 10)
-        self.assertEqual(hash_map['2'], 20)        
+        self.assertEqual(hash_map['2'], 20)
 
 if __name__ == '__main__':
     unittest.main()

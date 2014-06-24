84edf87f92d930f34db9135beb934a3a206aa731
Alvin777 <alvin777@mail.ru>
2014-06-24 20:06:18 +0400

    [New] Реализован OpenAddressingHashMap

--- structures/test_hash_map.py
+++ structures/test_hash_map.py
@@ -13,7 +13,7 @@ def random_generator(size):
 
 
 class ChainedHashMap:
-    def __init__(self, allow_duplicates=True, initial_size=16, load_factor_threshold=5):
+    def __init__(self, allow_duplicates=False, initial_size=16, load_factor_threshold=5):
         self.allow_duplicates = allow_duplicates
         self.hash_array_size = initial_size
         # self.hash_array = array.array('L', [0 for _ in xrange(self.hash_array_size)])
@@ -35,11 +35,12 @@ class ChainedHashMap:
         found_list = [value for (stored_key, value) in
                       self.hash_array[key_index] if stored_key == key]
 
-        if len(found_list) == 0:
-            return None
+        if not self.allow_duplicates:
+            if len(found_list) == 0:
+                return None
 
-        if len(found_list) == 1:
-            return found_list[0]
+            if len(found_list) == 1:
+                return found_list[0]
 
         return found_list
 
@@ -81,7 +82,7 @@ class ChainedHashMap:
             list_len_before = len(linked_list)
             linked_list[:] = [(stored_key, value) for (stored_key, value) in linked_list if not key == stored_key]
             len_diff = list_len_before - len(linked_list)
-            
+
             self.elements_count -= len_diff
             self.load_factor = self.elements_count/self.hash_array_size
 
@@ -105,7 +106,129 @@ class ChainedHashMap:
         return [len(x) if x is not None else 0 for x in self.hash_array]
 
 
-class TestSortingMethods(unittest.TestCase):
+class OpenAddressingHashMap:
+    def __init__(self, allow_duplicates=False, initial_size=16, load_factor_threshold=0.75):
+        self.allow_duplicates = allow_duplicates
+        self.hash_array_size = initial_size
+        # self.hash_array = array.array('L', [0 for _ in xrange(self.hash_array_size)])
+        self.hash_array = [None] * self.hash_array_size
+        self.elements_count = 0
+        self.load_factor = 0
+        self.load_factor_threshold = load_factor_threshold
+
+        self.deletedValueStub = ()
+
+    def calculate_hash(self, key):
+        return hash(key)
+
+    def __getitem__(self, key):
+        key_hash = self.calculate_hash(key)
+        initial_key_index = key_hash % self.hash_array_size
+
+        key_index = initial_key_index
+        resultsList = []
+        while True:
+            if self.hash_array[key_index] is None:
+                break
+
+            if self.hash_array[key_index] is not self.deletedValueStub and self.hash_array[key_index][0] == key:
+                if self.allow_duplicates:
+                    resultsList.append(self.hash_array[key_index][1])
+                else:
+                    return self.hash_array[key_index][1]
+
+            key_index += 1
+            key_index %= self.hash_array_size
+
+            if key_index == initial_key_index:   # one loop is enough
+                break
+
+        if self.allow_duplicates:
+            return resultsList
+
+        return None
+
+    def __setitem__(self, key, value):
+        key_hash = self.calculate_hash(key)
+        initial_key_index = key_hash % self.hash_array_size
+
+        # print 'setitem:', key, key_hash, self.hash_array_size, initial_key_index
+
+        key_index = initial_key_index
+        inserted = False
+        while True:
+            # print 'key_index:', key_index, 'initial_key_index:', initial_key_index, 'self.hash_array_size:', self.hash_array_size, \
+                  # 'self.hash_array:', self.hash_array
+
+            if self.hash_array[key_index] in [None, self.deletedValueStub]:
+                self.hash_array[key_index] = (key, value)
+                inserted = True
+                break
+            elif not self.allow_duplicates and self.hash_array[key_index][0] == key:
+                self.hash_array[key_index] = (key, value)
+                break
+
+            key_index += 1
+            key_index %= self.hash_array_size
+            if key_index == initial_key_index:
+                raise Exception("No more space")
+
+        if inserted:
+            self.elements_count += 1
+            self.load_factor = self.elements_count/self.hash_array_size
+
+            if self.load_factor >= self.load_factor_threshold:
+                self.resize(self.hash_array_size*2)
+
+    def __delitem__(self, key):
+        key_hash = self.calculate_hash(key)
+        initial_key_index = key_hash % self.hash_array_size
+
+        key_index = initial_key_index
+        deleted = False
+
+        while True:
+            # print 'key_index:', key_index, 'self.hash_array[key_index]:', self.hash_array[key_index]
+
+            if self.hash_array[key_index] is None:
+                break
+
+            if self.hash_array[key_index] is not self.deletedValueStub and self.hash_array[key_index][0] == key:
+                self.hash_array[key_index] = self.deletedValueStub
+
+                self.elements_count -= 1
+
+                if not self.allow_duplicates:
+                    break
+
+            key_index += 1
+            key_index %= self.hash_array_size
+
+            if key_index == initial_key_index:   # one loop is enough
+                break
+
+        self.load_factor = self.elements_count/self.hash_array_size
+
+    def size(self):
+        return self.elements_count
+
+    def resize(self, new_size):
+        # print 'resizing to', new_size
+        old_hash_array = self.hash_array
+
+        self.hash_array = [None] * new_size
+        self.hash_array_size = new_size
+        self.elements_count = 0
+        self.load_factor = 0
+
+        for (key, value) in [(key, value) for (key, value) in old_hash_array if (key, value) is not None]:
+            self[key] = value
+
+    # def stats(self):
+    #     return [len(x) if x is not None else 0 for x in self.hash_array]
+
+
+class TestChainedHashMap(unittest.TestCase):
     def test_simple_hash_map(self):
         hash_map = ChainedHashMap()
         hash_map[1] = 10
@@ -144,7 +267,7 @@ class TestSortingMethods(unittest.TestCase):
         # print hash_map.stats()
 
     def test_1000_elements_limited(self):
-        hash_map = ChainedHashMap(allow_duplicates=False)
+        hash_map = ChainedHashMap()
         reference_dict = {}
 
         for x in xrange(1000):
@@ -161,7 +284,7 @@ class TestSortingMethods(unittest.TestCase):
         # print hash_map.stats()
 
     def test_restrict_duplicates(self):
-        hash_map = ChainedHashMap(allow_duplicates=False)
+        hash_map = ChainedHashMap()
         hash_map[1] = 10
         hash_map[1] = 20
 
@@ -179,7 +302,7 @@ class TestSortingMethods(unittest.TestCase):
         self.assertEqual(hash_map[101], None)
 
     def test_deletion_restrict_duplicates(self):
-        hash_map = ChainedHashMap(allow_duplicates=False)
+        hash_map = ChainedHashMap()
         hash_map[1] = 10
         hash_map[101] = 20
         hash_map[101] = 30
@@ -198,5 +321,117 @@ class TestSortingMethods(unittest.TestCase):
         self.assertEqual(hash_map['1'], 10)
         self.assertEqual(hash_map['2'], 20)
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_duplicates(self):
+        hash_map = ChainedHashMap(allow_duplicates=True)
+        hash_map[1] = 10
+        hash_map[1] = 20
+
+        self.assertEqual(hash_map.size(), 2)
+        self.assertEqual(hash_map[1], [10, 20])
+        self.assertEqual(hash_map[2], None)
+
+
+class TestOpenAddressingHashMap(unittest.TestCase):
+    def test_simple_hash_map(self):
+        hash_map = OpenAddressingHashMap()
+        hash_map[1] = 10
+        hash_map[2] = 20
+
+        self.assertEqual(hash_map.size(), 2)
+        self.assertEqual(hash_map[1], 10)
+        self.assertEqual(hash_map[2], 20)
+        self.assertEqual(hash_map[3], None)
+        self.assertEqual(hash_map[17], None)
+
+    def test_simple_collision(self):
+        hash_map = OpenAddressingHashMap(initial_size=100)
+        hash_map[1] = 10
+        hash_map[101] = 20
+
+        self.assertEqual(hash_map.size(), 2)
+        self.assertEqual(hash_map[1], 10)
+        self.assertEqual(hash_map[101], 20)
+
+    def test_1000_elements(self):
+        hash_map = OpenAddressingHashMap()
+        reference_dict = {}
+
+        for x in xrange(1000):
+            key = int(random.random()*sys.maxint)
+            value = int(random.random()*sys.maxint)
+            reference_dict[key] = value
+            hash_map[key] = value
+
+        self.assertEqual(hash_map.size(), len(reference_dict))
+
+        for (key, value) in reference_dict.iteritems():
+            self.assertEqual(hash_map[key], value)
+
+        # print hash_map.stats()
+
+    def test_1000_elements_limited(self):
+        hash_map = OpenAddressingHashMap()
+        reference_dict = {}
+
+        for x in xrange(1000):
+            key = int(random.random()*1000)
+            value = int(random.random()*1000)
+            reference_dict[key] = value
+            hash_map[key] = value
+
+        self.assertEqual(hash_map.size(), len(reference_dict))
+
+        for (key, value) in reference_dict.iteritems():
+            self.assertEqual(hash_map[key], value)
+
+        # print hash_map.stats()
+
+    def test_restrict_duplicates(self):
+        hash_map = OpenAddressingHashMap()
+        hash_map[1] = 10
+        hash_map[1] = 20
+
+        self.assertEqual(hash_map.size(), 1)
+        self.assertEqual(hash_map[1], 20)
+
+    def test_deletion(self):
+        hash_map = OpenAddressingHashMap()
+        hash_map[1] = 10
+        hash_map[101] = 20
+        del hash_map[101]
+
+        self.assertEqual(hash_map.size(), 1)
+        self.assertEqual(hash_map[1], 10)
+        self.assertEqual(hash_map[101], None)
+
+    def test_deletion_restrict_duplicates(self):
+        hash_map = OpenAddressingHashMap()
+        hash_map[1] = 10
+        hash_map[101] = 20
+        hash_map[101] = 30
+        del hash_map[101]
+
+        self.assertEqual(hash_map.size(), 1)
+        self.assertEqual(hash_map[1], 10)
+        self.assertEqual(hash_map[101], None)
+
+    def test_string_keys(self):
+        hash_map = OpenAddressingHashMap()
+        hash_map['1'] = 10
+        hash_map['2'] = 20
+
+        self.assertEqual(hash_map.size(), 2)
+        self.assertEqual(hash_map['1'], 10)
+        self.assertEqual(hash_map['2'], 20)
+
+    def test_duplicates(self):
+        hash_map = OpenAddressingHashMap(allow_duplicates=True)
+        hash_map[1] = 10
+        hash_map[1] = 20
+
+        self.assertEqual(hash_map.size(), 2)
+        self.assertEqual(hash_map[1], [10, 20])
+        self.assertEqual(hash_map[2], [])
+
+# if __name__ == '__main__':
+#     unittest.main()
